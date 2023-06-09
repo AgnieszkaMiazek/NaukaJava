54482e286d708431cb8e6b92299c969ccf103248
AgnieszkaMiazek <123297831+AgnieszkaMiazek@users.noreply.github.com>
2023-06-09 20:50:02 +0200

    add wihout out

--- src/PracaDomowa3.java
+++ src/PracaDomowa3.java
@@ -1,3 +1,7 @@
+import com.sun.source.util.SourcePositions;
+
+import java.sql.SQLOutput;
+
 // wypisz liczby podzielne przez 3, w zakresie 1-100
 // odwróć elementy tablicy [1,3,5, 7, 0] -> [0,7,5,3,1]
 public class PracaDomowa3 {
@@ -14,6 +18,7 @@ public class PracaDomowa3 {
             int temp = numbers[i]; // dla i równego zero ma wartośc 1
             numbers[i] = numbers[numbers.length - 1 - i]; // 4 -> 0
             numbers[numbers.length - 1 - i] = temp; // 1
+            System.out.println("Iteracja numer " + i);
         }
         for (int i=0; i< numbers.length;i++){
             System.out.println(numbers[i]);
